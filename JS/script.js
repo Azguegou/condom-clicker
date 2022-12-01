c4ac58e6fb1f69c647e4aef2c7d3eb8bd3f951ec
Axel BRUN <brunax0302@iutparis.local>
2022-12-02 00:27:45 +0100

    hello

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,31 @@ var condom = document.getElementById("mainCondom");
 console.log(condom);
 var compteur = document.getElementById("nbCapotes");
 var valeur = 0;
+var rainDiv = document.getElementById("condomRain");
+
 condom.addEventListener("click", function(){
     valeur++;
     compteur.innerText = valeur;
 })
+setInterval(appendImage, 1000);
+
+function myTimer() {
+  const d = new Date();
+
+
+}
+
+function appendImage() {
+    var img = document.createElement('img');
+    console.log("aa");
+    img.setAttribute('src', 'images/capote_fond.png');
+    img.classList.add('capote');
+    img.style.left = Math.floor(Math.random() * 100) + 'vw';
+    rainDiv.appendChild(img);
+    setTimeout(deleteImage(img), 1000000);
+  }
+
+  function deleteImage(img) {
+    console.log("delete")
+    rainDiv.removeChild(img);
+  }
